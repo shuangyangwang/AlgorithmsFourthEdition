68e2eb50e09cb935d80e716f89f911bf074e2503
wangshuangyang <soton2014sky@126.com>
2018-10-20 11:58:11 +0800

    init

--- Graph/src/Graph.java
+++ Graph/src/Graph.java
@@ -1,2 +1,17 @@
+import edu.princeton.cs.algs4.Bag;
+
 public class Graph {
+
+    private final int V;
+    private int E;
+    private Bag<Integer>[] adj;
+
+
+    public Graph(int V) {
+        this.V = V;
+        this.adj = (Bag<Integer>[]) new Bag[V];
+        for(int v = 0; v < V; v++){
+            adj[v] = new Bag<Integer>();
+        }
+    }
 }
